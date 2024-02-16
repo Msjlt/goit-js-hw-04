78b2ce90b7617ad23d959a2e75b22a835bfd6e3c
Msjlt <151677911+Msjlt@users.noreply.github.com>
2024-02-16 14:38:16 +0300

    Update task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,19 @@
 "use strict";
 
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
+
   let totalCalories = 0;
 
   for (const day of days) {
     totalCalories += day.calories;
   }
 
-  return totalCalories / days.length;
+  const averageCalories = totalCalories / days.length;
+
+  return averageCalories;
 }
 
 console.log(
